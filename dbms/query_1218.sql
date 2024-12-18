7124546c8ef5125326e4576c73086de8c78a1a86
carami <carami@nate.com>
2024-12-18 11:53:26 +0900

    update exam

--- dbms/query_1218.sql
+++ dbms/query_1218.sql
@@ -49,5 +49,37 @@ select job, count(*) from emp group by job;
 -- job별 사원수와, 평균급여 를 알고 있어요. 
 select job, count(*), avg(sal), sum(sal) from emp group by job;
 
+-- 20번 부서의 job별 평균급여를 알고 싶어요.    -- 조건 (전체 데이터에대한 조건은 where.. ) 
+select job, avg(sal) from emp where deptno = 20 group by job;
+
+-- 부서별 총 급여액이 9000 이 넘는 부서만 보고 싶어요.   -- 조건 (대상.. 그룹핑한 데이터 having) 
+select deptno, sum(sal) from emp group by deptno having sum(sal) > 9000;
+
+-- 부서별 평균 금액을 알고 싶어요.  단 부서별 평균 급여가 2500 보다 작은 부서만 보고 싶고, 급여가 4500 이상거나, 1000 미만인 사원은 제외하고 평균급여를 구해주세요.
+select deptno, avg(sal) from emp where sal < 4500 group by deptno having avg(sal) < 2500; 
+select deptno, avg(sal) from emp where sal < 4500 and sal >= 1000 group by deptno having avg(sal) < 2500; 
+select deptno, avg(sal) a from emp where sal < 4500 and sal >= 1000 group by deptno having a < 2500; 
+
+select avg(sal) from emp group by deptno;
+
+--  사원 정보를 알고 싶어요.  사원번호, 이름, 부서명, 급여 정보를 알고 싶어요.  
+select empno, ename, dname ,sal from emp, dept;
+-- join 조건 
+select empno, ename, dname ,sal from emp, dept where emp.deptno = dept.deptno;
+
+
+-- deptno 를 출력하니 오류가 발생!!  왜? 부서번호가 몇개 있나요? deptno  
+select empno, ename,emp.deptno, dname ,sal from emp, dept where emp.deptno = dept.deptno;
+
+-- 각 컬럼의 이름앞에 어떤 테이블의 컬럼인지를 명시적으로 쓰는게 좋다. 
+select e.empno, e.ename,e,deptno, d.dname ,e.sal from emp e, dept d where e.deptno = d.deptno;
+
+
+
+desc dept;
+select * from dept;
+select * from emp;
+
+select * from emp, dept;
 
 
