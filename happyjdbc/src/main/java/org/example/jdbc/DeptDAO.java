0cc606958f8175d9cff2fb650b947476639faf48
carami <carami@nate.com>
2024-12-20 14:29:47 +0900

    update exam

--- happyjdbc/src/main/java/org/example/jdbc/DeptDAO.java
+++ happyjdbc/src/main/java/org/example/jdbc/DeptDAO.java
@@ -1,12 +1,40 @@
 package org.example.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class DeptDAO {
     //기본 CRUD
     //insert  -- c
     //dept 테이블에 데이터를 한 건 입력하는 메소드를 정의하세요.
     public boolean addDept(DeptDTO deptDTO){
+        boolean flag = false;
+        //1.필요한 객체선언
+        Connection conn = null;
+        PreparedStatement ps = null;
+        String sql = "insert into dept(deptno,dname,loc) values(?,?,?)";
+        try{
+            //3. 접속
+            conn = DBUtil.getConnection();
+            //4. 쿼리작성
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1,deptDTO.getDeptno());
+            ps.setString(2, deptDTO.getDname());
+            ps.setString(3,deptDTO.getLoc());
 
-        return false;
+            //5.쿼리실행
+            int count = ps.executeUpdate();
+
+            if(count == 1)
+                flag = true;
+
+        }catch (Exception e){
+            System.out.println(e);
+        }finally {
+            //2. 선언한 객체 닫기
+            DBUtil.close(conn,ps);
+        }
+        return flag;
     }
 
     //update    -- u
