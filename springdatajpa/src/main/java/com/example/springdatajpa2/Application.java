798782a35631b4425e20b2b9543fbbd836aa662c
carami <carami@nate.com>
2025-02-19 15:52:49 +0900

    update exam

--- springdatajpa/src/main/java/com/example/springdatajpa2/Application.java
+++ springdatajpa/src/main/java/com/example/springdatajpa2/Application.java
@@ -5,9 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.List;
@@ -42,15 +41,46 @@ public class Application {
             customer1.forEach(customer -> log.info("이름으로 조회 :: {} , {} ",customer.getName(), customer.getEmail()));
 
             //이메일로 조회
+            repository.findByEmail("kim@example.com").
+                    forEach(customer -> log.info("이메일로 조회 :: {} , {} ", customer.getName(),customer.getEmail()));
 
             //이메일에 특정 문자열을 포함 하고 있는 고객 조회
+            log.info("이메일에 특정 문자열을 포함 하고 있는 고객 조회");
+            repository.findByEmailContaining("exam")
+                    .forEach(customer -> log.info("이메일로 조회 :: {} , {} ", customer.getName(),customer.getEmail()));
 
             //각 고객과 고객의 주문수를 알고 싶어요.
 //            @Query("SELECT c, count(o) FROM Customer c LEFT JOIN c.orders o GROUP BY c")
+            log.info("각 고객과 고객의 주문수를 알고 싶어요.");
+            List<Object[]> customerOrderCounts = repository.findCustomerOrderCount();
+            log.info("결과 ::::  "+customerOrderCounts.size());
+            customerOrderCounts.forEach(result->{
+                Customer customer = (Customer) result[0];
+                Long count = (Long)result[1];
+                log.info("고객 :: "+ customer.getName()+ ",  주문 수 :: "+ count);
+            });
             //고객의 세부 정보와 그 고객의 가장 최근 주문 조회를 알고 싶어요.
 //            @Query("SELECT c, o FROM Customer c JOIN c.orders o WHERE o.date = (SELECT MAX(o2.date) FROM Order o2 WHERE o2.customer = c")
+           log.info("고객의 세부 정보와 그 고객의 가장 최근 주문 조회를 알고 싶어요.");
+            List<Object[]> latestOrders = repository.findCustomersWithLatestOrder();
+            latestOrders.forEach(result ->{
+                Customer customer = (Customer) result[0];
+                Order order=(Order) result[1];
+                log.info("고객 ::  {} , 최근 주문 :: {}", customer.getName(), order.getProduct());
+            });
             //평균나이보다 많은 고객을 조회하고 싶어요.
 //            @Query("SELECT c FROM Customer c WHERE c.age > (SELECT avg(c2.age) FROM Customer c2)")
+            log.info("평균나이보다 많은 고객을 조회하고 싶어요.");
+            repository.findCustomersOlderThanAverage()
+                    .forEach(customer -> log.info("평균나이 이상의 고객 :: "+customer.getName()));
+
+
+            Pageable pageable = PageRequest.of(0,3);
+            repository.findByEmailContaining("exam",pageable)
+                    .forEach(customer -> log.info("페이징 처리된 customer :: {},{}",customer.getName(),customer.getEmail()));
+
+
         };
+
     }
 }
--- springdatajpa/src/main/java/com/example/springdatajpa2/CustomerRepository.java
+++ springdatajpa/src/main/java/com/example/springdatajpa2/CustomerRepository.java
@@ -1,6 +1,8 @@
 package com.example.springdatajpa2;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,13 +13,20 @@ public interface CustomerRepository extends JpaRepository<Customer,Long> {
     List<Customer> findByName(String name);
 
     //이메일로 조회
-
+    List<Customer> findByEmail(String email);
     //이메일에 특정 문자열을 포함 하고 있는 고객 조회
+    List<Customer> findByEmailContaining(String email);
+
+    List<Customer> findByEmailContaining(String email, Pageable pageable);
 
     //각 고객과 고객의 주문수를 알고 싶어요.
-
-    //고객의 세부 정보와 그 고객의 가장 최근 주문 조회를 알고 싶어요.
-
-    //평균나이보다 많은 고객을 조회하고 싶어요.
+    @Query("SELECT c, count(o) FROM Customer c LEFT JOIN c.orders o GROUP BY c")
+    List<Object[]> findCustomerOrderCount();
+//    //고객의 세부 정보와 그 고객의 가장 최근 주문 조회를 알고 싶어요.
+    @Query("SELECT c, o FROM Customer c JOIN c.orders o WHERE o.date = (SELECT MAX(o2.date) FROM Order o2 WHERE o2.customer = c)")
+    List<Object[]> findCustomersWithLatestOrder();
+//    //평균나이보다 많은 고객을 조회하고 싶어요.
+    @Query("SELECT c FROM Customer c WHERE c.age > (SELECT avg(c2.age) FROM Customer c2)")
+    List<Customer> findCustomersOlderThanAverage();
 
 }
