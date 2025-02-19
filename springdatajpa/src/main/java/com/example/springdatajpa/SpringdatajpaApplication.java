1b2200b742dad73ee7d99e56430e4b2c78da7696
carami <carami@nate.com>
2025-02-19 11:53:24 +0900

    update exam

--- springdatajpa/src/main/java/com/example/springdatajpa/SpringdatajpaApplication.java
+++ springdatajpa/src/main/java/com/example/springdatajpa/SpringdatajpaApplication.java
@@ -1,8 +1,17 @@
 package com.example.springdatajpa;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
 @SpringBootApplication
 public class SpringdatajpaApplication {
 
@@ -10,4 +19,50 @@ public class SpringdatajpaApplication {
 		SpringApplication.run(SpringdatajpaApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner run(UserRepository repository){
+		return args -> {
+			//create
+//			User user = new User("Hong","hong@example.com");
+//			repository.save(user);
+//			log.info("사용자 추가 :: "+user);
+
+			//delete
+//			User user = repository.findById(15L).get();
+//			repository.delete(user);
+
+			//update
+//			User user = repository.findById(11L).get();
+//			user.setName("강경미");
+//			user.setEmail("carami@nate.com");
+//
+//			repository.save(user);
+
+//			repository.findAll().forEach(user -> log.info(user.toString()));
+
+//			List<User> caramis = repository.findByName("carami");
+////			caramis.forEach(user -> user.toString());
+//			for(User user : caramis){
+//				log.info(user.toString());
+//			}
+
+//			repository.findByName("kang").forEach(user -> log.info(user.toString()));
+
+//			repository.selectUser("kang").forEach(user -> log.info(user.toString()));
+//			repository.selectUserLike("kan").forEach(user -> log.info(user.toString()));
+
+//			int count = repository.deleteByEmail("kang2@exam.com");
+//
+//			log.info("delete count :: "+ count);
+
+//			int updateCount = repository.updateByEmail(9L, "kang@gmail.com");
+//			log.info("update count :: "+ updateCount);
+
+//			repository.selectByEmailNative("exam").forEach(user -> log.info(user.toString()));
+			Pageable pageable = PageRequest.of(0, 3);
+			repository.findByNameContaining("a",pageable ).forEach(user -> log.info(user.toString()));
+			log.info("=======================");
+			repository.findAll(pageable).forEach(user -> log.info(user.toString()));
+		};
+	}
 }
--- springdatajpa/src/main/java/com/example/springdatajpa/UserRepository.java
+++ springdatajpa/src/main/java/com/example/springdatajpa/UserRepository.java
@@ -1,6 +1,48 @@
 package com.example.springdatajpa;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    //이름을 기준으로 조회하고 싶어요.  어떻게 메서드를 정의하면 될까요?
+    List<User> findByName(String name);
+    List<User> findByEmail(String email);
+    List<User> findByNameContaining(String name); //select * from jpa_user where name like %name%
+    List<User> findByNameAndEmail(String name, String email);
+    List<User> findByNameOrEmail(String name, String email);
+
+    //select * from user where name = name and email = email
+    //select * from user where create_at Between start and end ;
+    //jpql  vs  sql   --   차이점
+    //jpql --  대상 : 엔티티
+    //sql  --  대상 : table
+    @Query("SELECT u FROM User u WHERE u.name = :name")
+    List<User> selectUser(@Param("name")String name);
+    @Query("SELECT u FROM User u WHERE u.name LIKE %:name%")
+    List<User> selectUserLike(@Param("name") String name);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.email = :email")
+    int deleteByEmail(@Param("email")String email);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE User u SET u.email = :email WHERE u.id=:id")
+    int updateByEmail(@Param("id")Long id, @Param("email")String email);
+
+
+    @Query(nativeQuery = true, value = "SELECT * FROM jpa_user WHERE email like %:email%")
+    List<User> selectByEmailNative(@Param("email")String email);
+
+    @Query(value = "SELECT * FROM jpa_user WHERE email like %?1%", nativeQuery = true)
+    List<User> selByEmail(String email);
+
+    List<User> findByNameContaining(String name, Pageable pageable);
 }
