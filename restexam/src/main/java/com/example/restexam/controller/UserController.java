54573db874c0a6947e58c840da8fa66f860b5245
carami <carami@nate.com>
2025-02-24 11:55:04 +0900

    update exam:

---
+++ restexam/src/main/java/com/example/restexam/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.example.restexam.controller;
+
+public class UserController {
+    //http://localhost:8080/api/users       -- GET
+    //http://localhost:8080/api/users       -- POST
+    //http://localhost:8080/api/users/{id}       -- GET
+    //http://localhost:8080/api/users/{id}       -- PUT
+    //http://localhost:8080/api/users/{id}       -- DELETE
+
+    //위에 제시한 것들이 실행되도록  UserController 를 완성해보세요.
+
+}
